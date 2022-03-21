003167b4db50105463a4d66cefa87f543eb664ce
Edy Segura <edysegura@gmail.com>
2022-03-21 18:39:11 -0300

    chore: add new test cases for seconds to hh:mm:ss

--- using-es-modules/src/secondsToHms/secondsToHms.test.js
+++ using-es-modules/src/secondsToHms/secondsToHms.test.js
@@ -4,5 +4,7 @@ describe('Seconds to hh:mm:ss', () => {
   it('it should sum two numbers', () => {
     expect(secondsToHms(0)).toBe('00:00:00')
     expect(secondsToHms(1)).toBe('00:00:01')
+    expect(secondsToHms(60)).toBe('00:01:00')
+    expect(secondsToHms(60)).toBe('01:00:00')
   })
 })
