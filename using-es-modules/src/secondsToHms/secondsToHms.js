6eb0c2a18fd4958a04b9eb864df4f5ffcd760029
Edy Segura <edysegura@gmail.com>
2023-10-18 21:15:55 -0300

    chore: remove empty spaces

--- using-es-modules/src/secondsToHms/secondsToHms.js
+++ using-es-modules/src/secondsToHms/secondsToHms.js
@@ -2,8 +2,6 @@ export function secondsToHms(elapsedSeconds) {
   const hours = Math.floor(elapsedSeconds / 3600)
   const minutes = Math.floor((elapsedSeconds % 3600) / 60)
   const seconds = Math.floor((elapsedSeconds % 3600) % 60)
-
   const pad = (value) => String(value).padStart(2, '0')
-
   return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
 }
